1f4fbfd17a4b57d9bbf1b45780d2dab2300ca529
Russell Snyder <russell@rustybass.com>
2019-06-15 22:08:18 +0200

    Initial Commit

---
+++ Dogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dogs
+//
+//  Created by Russell Wunder on 15.06.19.
+//  Copyright © 2019 Russell Snyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
